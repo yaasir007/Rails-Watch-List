c2aea6a11fd7547b9360d49678ae3197b526ba79
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-07 05:10:12 +0400

    Model validations completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_165449) do
+ActiveRecord::Schema.define(version: 2021_11_07_004322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
